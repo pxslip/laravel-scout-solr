82119a9f8f643de256d8a4e3ebfbb8105604c6ba
Gilles Crettenand <gilles.crettenand@liip.ch>
2018-12-07 09:06:41 +0100

    Various small fixes: phpdoc, typehinting

--- src/Searchable.php
+++ src/Searchable.php
@@ -21,7 +21,7 @@ trait Searchable
      *
      * @param  string  $query
      * @param  Closure  $callback
-     * @return Scout\Solr\Builder
+     * @return \Scout\Solr\Builder
      */
     public static function search($query, $callback = null)
     {
